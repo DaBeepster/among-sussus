50754e5dcd23943fc5c3a9050a06b9f8377473c8
Glitch (among-sussus) <none>
2024-05-02 16:58:23 +0000

    🕒🐈‍⬛ Updated with Glitch

--- grr.js
+++ grr.js
@@ -28,3 +28,43 @@ function loadFive() {
   var yesh = localStorage.fiveSave;
   area.value = yesh;
 }
+function saveSix() {
+  var areea = document.getElementById('heh');
+  var contente = areea.value;
+  localStorage.sexthSave = contente;
+}
+function loadSix() {
+  var areea = document.getElementById('heh');
+  var yeet = localStorage.sexthSave;
+  areea.value = yeet;
+}
+function saveSeven() {
+  var ahe = document.getElementById('heh');
+  var stuf = ahe.value;
+  localStorage.sevenSave = stuf;
+}
+function loadSeven() {
+  var ahe = document.getElementById('heh');
+  var poh = localStorage.sevenSave;
+  ahe.value = poh;
+}
+function saveTemp() {
+  var gyatt = document.getElementById('heh');
+  var skibidi = gyatt.value;
+  localStorage.tempSave = skibidi;
+}
+function loadTemp() {
+  var ohio = document.getElementById('heh');
+  var sigma = localStorage.tempSave;
+  ohio.value = sigma;
+}
+function saveImp() {
+  var box = document.getElementById('heh');
+  var content = box.value;
+  localStorage.importantSave = content;
+}
+function loadImp() {
+  var box = document.getElementById('heh');
+  var savedcontent = localStorage.importantSave;
+  box.value = savedcontent;
+}
